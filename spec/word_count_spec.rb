0a90043e36b2075d49d1da19876d88e71e7dacd2
Vaughn Dice <donvice@gmail.com>
2015-08-07 09:47:33 -0700

    specs added around multiple occurences, also whitespace occurences

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -9,4 +9,16 @@ describe('String#word_count') do
   it('accepts the string \'hello world\' and key \'world\' and returns 1') do
     expect('hello world'.word_count('world')).to(eq(1))
   end
+
+  it('accepts the string \'1 1 1 1 1\' and key \'1\' and returns 5') do
+    expect('1 1 1 1 1'.word_count('1')).to(eq(5))
+  end
+
+  it('accepts the string \'1 1 1 1 1\' and key \' \' and returns 4') do
+    expect('1 1 1 1 1'.word_count(' ')).to(eq(4))
+  end
+
+  it('accepts the string \'11111\' and key \'1\' and returns 5') do
+    expect('11111'.word_count('1')).to(eq(5))
+  end
 end
