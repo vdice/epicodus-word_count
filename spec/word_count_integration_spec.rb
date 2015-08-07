11ca12c85383d86921bc5b64fb97713f5f2fd195
Vaughn Dice <donvice@gmail.com>
2015-08-07 11:13:30 -0700

    refactored integration spec to use constants

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -7,14 +7,18 @@ describe('the result path', {:type => :feature}) do
   PETER_PIPER_STRING =
     'If Peter Piper picked a peck of pickled peppers,' +
     ' where\'s the peck of pickled peppers Peter Piper picked?'
+  PETER_PIPER_KEY = 'peck'
+  EXPECTED_COUNT = 2
 
-  it('processes the input, searching for occurences of key and returns the result') do
+  it('processes the input, searching for occurrences of key and returns the result') do
     visit('/')
     expect(page).to have_content("Word Count")
     fill_in('input', :with => PETER_PIPER_STRING)
-    fill_in('key', :with => 'peck')
+    fill_in('key', :with => PETER_PIPER_KEY)
     click_button('Submit')
-    expect(page).to have_content('2')
+    expect(page).to have_content(PETER_PIPER_STRING)
+    expect(page).to have_content(PETER_PIPER_KEY)
+    expect(page).to have_content(EXPECTED_COUNT)
   end
 
   it('returns users to index', {:type => :feature}) do
