2cb4ca5511a6cf9c7e5df23cf33e618363be824a
Vaughn Dice <donvice@gmail.com>
2015-08-07 09:43:11 -0700

    next spec added checking correct count of word 'world' in 'hello world'

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,4 +5,8 @@ describe('String#word_count') do
   it('accepts user input') do
     expect('user input'.word_count('')).to(eq(0))
   end
+
+  it('accepts the string \'hello world\' and key \'world\' and returns 1') do
+    expect('hello world'.word_count('world')).to(eq(1))
+  end
 end
